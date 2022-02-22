2693319377ad61850e73807f1d6ae54ff9bc46ce
Erick <erickferreira1805@gmail.com>
2022-02-22 17:50:08 -0300

    Atualizando Classe model Categoria

--- Pv1 - 22-02-2022/prodigio/src/main/java/com/generation/prodigio/model/Categoria.java
+++ Pv1 - 22-02-2022/prodigio/src/main/java/com/generation/prodigio/model/Categoria.java
@@ -11,19 +11,18 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name="tb_categoria")
 public class Categoria {
-	
-	
+		
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long id; 
 	
 	@NotBlank(message="Este campo é obrigatório.")
 	@Size(min=5, max=255, message="Este campo deverá ter o máximo de 20 caracteres.")
-	private String areaDeEstudo;
+	private String materia; /*Puxa o assunto que quer estudar.*/
 	
 	@NotBlank(message="Este campo é obrigatório.")
 	@Size(min=5, max=255, message="Este campo deverá ter o máximo de 20 caracteres.")
-	private String materia;
+	private String areaDeEstudo; /*Puxa o conteudo a ser estudado*/
 
 	public Long getId() {
 		return id;
@@ -33,19 +32,18 @@ public class Categoria {
 		this.id = id;
 	}
 
-	public String getAreaDeEstudo() {
-		return areaDeEstudo;
-	}
-
-	public void setAreaDeEstudo(String areaDeEstudo) {
-		this.areaDeEstudo = areaDeEstudo;
-	}
-
 	public String getMateria() {
 		return materia;
 	}
 
 	public void setMateria(String materia) {
 		this.materia = materia;
+	}
+	public String getAreaDeEstudo() {
+		return areaDeEstudo;
+	}
+
+	public void setAreaDeEstudo(String areaDeEstudo) {
+		this.areaDeEstudo = areaDeEstudo;
 	}
 }
