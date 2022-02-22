bfe489ff5a0e0e3e19818909e5a3ff12f6ce177c
Erick <erickferreira1805@gmail.com>
2022-02-22 11:01:54 -0300

    Criando Projeto

---
+++ Pv1 - 22-02-2022/prodigio/src/main/java/com/generation/prodigio/ProdigioApplication.java
@@ -0,0 +1,13 @@
+package com.generation.prodigio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdigioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdigioApplication.class, args);
+	}
+
+}
